cdb1776171b31021b7c22135d0b840fa6f1cb0f4
Saara Korhonen <saara.korhonen@courses.tamk.fi>
2017-10-11 12:41:11 +0300

    Platform generator

---
+++ projekti/Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour 
+{
+	public GameObject platform;
+
+	void Start () 
+	{
+		InvokeRepeating ("Spawn", 0, 1);
+	}
+
+	void Spawn ()
+	{
+		Instantiate (platform, new Vector2 (Random.Range (5.7f, -5.7f), 5.6f), Quaternion.identity);
+	}
+}
