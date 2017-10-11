68a1dbc52fa12f0c323ed7bf9bd5e3790684603e
Saara Korhonen <saara.korhonen@courses.tamk.fi>
2017-10-11 11:53:05 +0300

    Taustan liikutuksen alottelua

---
+++ projekti/Assets/Scripts/MoveThings.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveThings : MonoBehaviour 
+{
+	void Update () 
+	{
+		this.transform.Translate (new Vector2 (0, CameraPan.panSpeed*-Time.deltaTime));
+	}
+}
