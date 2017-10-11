4ed1a124690c31388a62b854f32a55f7a73a9f63
Saara Korhonen <saara.korhonen@courses.tamk.fi>
2017-10-11 12:19:42 +0300

    Background movement finished

---
+++ projekti/Assets/Scripts/MoveBG.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBG : MonoBehaviour 
+{
+	float moveSpeed = 1.5f;
+	Vector2 returnPos = new Vector2 (0, 25);
+
+	void Update () 
+	{
+		transform.Translate(new Vector2 (0, moveSpeed * -Time.deltaTime));
+		if (this.transform.position.y < -15) 
+		{
+			this.transform.position = returnPos;
+		}
+	}
+}
