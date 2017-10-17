eb91523c7ba360ef60b282efbafc34f9bbcce6d4
Heikki Kettunen <heikki.kettunen@courses.tamk.fi>
2017-10-17 10:09:41 +0300

    Lissee platform-combojen muokkausta.

---
+++ projekti/Assets/Scripts/Reload.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reload : MonoBehaviour 
+{
+	
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.U)) 
+		{
+			Application.LoadLevel (2);
+		}
+	}
+}
