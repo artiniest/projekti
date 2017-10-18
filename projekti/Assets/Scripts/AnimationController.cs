c23a9f94a65420c9bae00c9e1eaedd75e0331dbc
Teemu Anttila <teemu.anttila@courses.tamk.fi>
2017-10-18 12:37:27 +0300

    Muokatut seinäspritet, epätoivoista animaatio-skriptausta JOKA EI TOIMI YHYY

---
+++ projekti/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour 
+public class PlayStop : MonoBehaviour
+
+{
+	public GameObject[] objektit;
+	public GameObject velho;
+	public GameObject player;
+
+	{
+		public GameObject Table;
+
+		void PS()
+		{
+			Table.GetComponent<Animation>().Stop();
+		}
+	}
+
+	void Start()
+	{
+		public GameObject Table;
+
+		void PS()
+		{
+			Table.GetComponent<Animation>().Stop();
+		}
+	}
+
+	void Stop()
+	{
+		
+	}
+}
