7cafdf456c98ac6d02b4952ec0770566042ca824
Saara Korhonen <saara.korhonen@courses.tamk.fi>
2017-10-20 14:19:58 +0300

    ?

---
+++ projekti/Assets/Delete.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delete : MonoBehaviour 
+{
+	void Update ()
+	{
+		if (this.gameObject.transform.position.y < -20) 
+		{
+			Destroy (this.gameObject);
+		}
+	}
+}
