fd90ae0b16a8d3a9716a89b85ba24ebcc73c454c
Saara Korhonen <saara.korhonen@courses.tamk.fi>
2017-10-20 10:32:16 +0300

    Muutoksia

---
+++ projekti/Assets/Scripts/LoadGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadGame : MonoBehaviour 
+{
+	public int LevelToLoad;
+	float timeElapsed;
+
+	void Update ()
+	{
+		print (timeElapsed);
+		timeElapsed++;
+	}
+
+}
